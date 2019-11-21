40b9ace18e18a9879fba38a535ee50412abb388d
Vegitojor <j.velazquez1985@gmail.com>
2019-11-21 18:30:18 -0300

    cambio pass en proceso

--- incluciones/navbarVistas.php
+++ incluciones/navbarVistas.php
@@ -46,10 +46,34 @@
                         <?php if($_SESSION['usuario']['supervisor']==0){ ?>
                             <li role="presentation"><a href="./pedido.php">Mis pedidos</a></li>
                         <?php } ?>
+                        <li role="presentation"><a href="#" data-toggle="modal" data-target="#passModal">Cambiar contrase&ntilde;a</a></li>
                         <li role="presentation"><a href="../controladores/cerrarSesionController.php">Salir</a></li>
                     </ul>
                 </li>
             </ul>
+        </div>
+    </div>
+
+    <div id="myModal" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+
+            <!-- Modal content-->
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">Cambio de contrase&ntilde;a</h4>
+                </div>
+                <div class="modal-body">
+                    <p>Some text in the modal.</p>
+                    <form action="">
+                        <div class=""></div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+
         </div>
     </div>
     <!-- /.container -->
